d9ba83fd835fba29b3c11119ce92950d6834488e
Crvesoft <131706372+Crvesoft@users.noreply.github.com>
2025-04-03 02:05:47 +0800

    Update and rename rock6ball.js to rock8ball.js

--- rock8ball.js
+++ rock8ball.js
@@ -137,7 +137,7 @@
         canvas.style.position = 'fixed';
         canvas.style.top = '0';
         canvas.style.left = '0';
-        canvas.style.zIndex = '-1';
+        canvas.style.zIndex = '0';
         canvas.style.pointerEvents = 'none';
         
         // 初始设置
